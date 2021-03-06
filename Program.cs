7425571ed5abb742eaec9fc7191d395b1fea3f00
Danil Golybev <58464440+danilksk@users.noreply.github.com>
2021-03-06 16:34:37 +0300

    Add files via upload
    Для влюбленных пар...

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LoveAppConsole
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //Console.Wri
+            Console.Write("Введите число повторений ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите сообщение ");
+            string message = Convert.ToString(Console.ReadLine());
+           
+           for(int i = 1; i <= number; i++)
+            {
+                Console.WriteLine($"{i}.{message}", i);
+            }
+            
+            
+        }
+    }
+}
